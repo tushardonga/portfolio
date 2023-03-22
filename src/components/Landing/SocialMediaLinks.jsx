3dc799f72f39af11a67b5315de85f112e04d5847
Tushar Donga <tushar.donga@gtcsys.com>
2023-03-22 19:59:26 +0530

    added social media links

--- src/components/Landing/SocialMediaLinks.jsx
+++ src/components/Landing/SocialMediaLinks.jsx
@@ -4,19 +4,22 @@ import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 const SocialMediaLinks = () => {
   return (
     <div className="m-12 w-full flex items-center">
-      <a href="#" className="group">
+      <a
+        href="https://www.linkedin.com/in/tushar-donga-1091b416a/"
+        className="group"
+      >
         <FaLinkedin
           className="text-4xl mx-4 animate-bounce text-secondary"
           aria-label="LinkedIn"
         />
       </a>
-      <a href="#">
+      <a href="https://github.com/tushardonga">
         <FaGithub
           className="text-4xl mx-4 animate-bounce text-secondary"
           aria-label="GitHub"
         />
       </a>
-      <a href="#">
+      <a href="https://twitter.com/0786tushar">
         <FaTwitter
           className="text-4xl mx-4 animate-bounce text-secondary"
           aria-label="Twitter"
